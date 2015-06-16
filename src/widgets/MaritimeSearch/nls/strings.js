c8faafe685c755f083400f8e65e93c6b05a9dc72
Jaisudha Purushothaman <jpurushothaman@esri.com>
2015-06-15 18:01:09 -0700

    coming soon on search

--- src/widgets/MaritimeSearch/nls/strings.js
+++ src/widgets/MaritimeSearch/nls/strings.js
@@ -1,7 +1,8 @@
 define({
   root: {
     widgetTitle: "Maritime Search" ,
-    description: "Custom Maritime Chart Service Search"
+    //description: "Custom Maritime Chart Service Search"
+    description: "Coming soon"
   }
   // add supported locales below:
   // , "zh-cn": true
